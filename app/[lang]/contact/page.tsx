2e5cafcb326b9944badcda6f55bd8ca94c8de500
kadyr2601 <2601kadyr@gmail.com>
2024-10-31 16:48:46 +0100

    prod v1

--- app/[lang]/contact/page.tsx
+++ app/[lang]/contact/page.tsx
@@ -4,7 +4,9 @@ import SingleBanner from "@/components/layout/SingleBanner";
 import {PageBanner} from "@/components/DTOs";
 import contactSBanner from "@/public/contacts-Banner.jpg";
 
-const Page = (params: { lang: string }) => {
+interface PageProps { params: { lang: string } }
+
+const Page = ({ params: { lang } }: PageProps) => {
 
     const banner: PageBanner = {
         id: 1,
@@ -22,7 +24,7 @@ const Page = (params: { lang: string }) => {
         <div className={'contacts-page'}>
             <MainBanner pathname={"contact"}/>
 
-            <SingleBanner props={banner} lang={params.lang}/>
+            <SingleBanner props={banner} lang={lang}/>
         </div>
     );
 };
--- next.config.js
+++ next.config.js
@@ -1,19 +1,26 @@
 
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     images: {
         remotePatterns: [
             {
+                // protocol: 'http',
+                // hostname: '127.0.0.1',
+                // port: '8000',
+                // pathname: '/media/**',
+
                 protocol: 'http',
-                hostname: '127.0.0.1',
-                port: '8000',
+                hostname: '185.185.69.163',
+                port: '',
                 pathname: '/media/**',
             },
         ],
     },
     env: {
-        API_URL: "http://127.0.0.1:8000",
-        HostName: "localhost:3000"
+        // API_URL: "http://127.0.0.1:8000",
+        // HostName: "localhost:3000"
+        API_URL: "http://185.185.69.163",
+        HostName: "http://185.185.69.163:3000"
     },
 }
 
